ef13b34efb02000cc0221c7f135293ab189cc3dc
giacomo gramaccioni <giacomogramaccioni@gmail.com>
2024-02-27 12:08:04 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -73,7 +73,7 @@ class Dev(Configuration):
   # SECURITY WARNING: don't run with debug turned on in production!
   DEBUG = values.BooleanValue(True)
 
-  ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sonarcabinet-pastelvoice-8000.codio.io']
+  ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'controlchaos-llamatrilogy-8000.codio.io']
   X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
   CSRF_COOKIE_SAMESITE = None
   CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
--- blango/urls.py
+++ blango/urls.py
@@ -2,14 +2,16 @@ import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+import blango_auth.views
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("ip/", blog.views.get_ip)
+    path("ip/", blog.views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
 ]
 
 if settings.DEBUG:
